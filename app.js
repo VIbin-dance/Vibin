a8ef7e4b31916dfa8e5abc1ab38096646cdb7196
Shota Inoue <shota.inoue@vibin.tokyo>
2020-06-28 02:58:41 +0900

    attempting to deploy

--- app.js
+++ app.js
@@ -98,4 +98,6 @@ app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on ${PORT}`))
+app.listen(PORT, () => {
+    console.log('Server started on ' + port );
+});
