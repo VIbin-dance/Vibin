53169d97760d5afbb5abd4d8fa1b590ff8c81e5c
Shota Inoue <shota.inoue@vibin.tokyo>
2020-07-03 02:44:09 +0900

    created only allowed to dev feature

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,15 @@ const passport = require('passport');
 const { ensureAuthenticated } = require('../config/auth');
 
 const Video = require('../models/Video');
+const User = require('../models/User');
 
 router.get('/', (req, res) => {
   if (req.session.passport) {
     req.flash('success_msg', 'You are logged in!');
     res.redirect('/dashboard?page=1&limit=15');
-} else {
-    res.render('landing')}
+  } else {
+    res.render('landing')
+  }
 });
 
 router.get('/logout', (req, res) => {
@@ -22,18 +24,18 @@ router.get('/logout', (req, res) => {
 
 router.get('/auth/google', passport.authenticate('google', {
   scope: [
-      'https://www.googleapis.com/auth/userinfo.profile',
-      'https://www.googleapis.com/auth/userinfo.email',
-      'https://www.googleapis.com/auth/youtube',
-      'https://www.googleapis.com/auth/calendar'
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/youtube',
+    'https://www.googleapis.com/auth/calendar'
   ]
 }));
 
 // callback function
 router.get('/auth/google/callback',
   passport.authenticate('google', {
-      failureRedirect: '/error',
-      session: true
+    failureRedirect: '/error',
+    session: true
   }),
   (req, res) => {
     req.flash('success_msg', 'You are logged in!')
@@ -46,7 +48,7 @@ router.get('/error', (req, res) => {
 });
 
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
-  Video.paginate({}, {page: req.query.page, limit: req.query.limit}, (err, result) => {
+  Video.paginate({}, { page: req.query.page, limit: req.query.limit }, (err, result) => {
     const title = [];
     const choreographer = [];
     const url = [];
@@ -75,7 +77,7 @@ router.get('/dashboard', ensureAuthenticated, (req, res) => {
       pageCount: result.pages,
       pages: paginate.getArrayPages(req)(3, result.pages, req.query.page)
     })
-    });
+  });
 });
 
 router.get('/results', ensureAuthenticated, (req, res) => res.render('results'));
@@ -88,14 +90,23 @@ router.get('/calendar', (req, res) => {
 });
 
 router.get('/upload', ensureAuthenticated, (req, res) => {
-  res.render('upload', {
-    API_key: process.env.API_key,
-    CLIENT_ID: process.env.CLIENT_ID
-  });
+  User.findOne({ googleId: req.user.id }, (err, result) => {
+    console.log(result)
+    console.log(result.email)
+    if(result.email == '925shota@gmail.com' || result.email == 'so.gorman@vibin.tokyo') {
+      res.render('upload', {
+        API_key: process.env.API_key,
+        CLIENT_ID: process.env.CLIENT_ID
+      });
+    } else {
+      req.flash('error_msg', "Sorry this is not yet available to users!");
+      res.redirect('/dashboard?page=1&limit=15');
+    }
+  })
 })
 
 router.get('/player/:id', ensureAuthenticated, (req, res) => {
-  Video.findOne({id: req.params.id}, (err, result) => {
+  Video.findOne({ id: req.params.id }, (err, result) => {
     res.render('player', {
       id: req.params.id,
       title: result.title,
@@ -120,7 +131,7 @@ router.post('/dashboard', (req, res) => {
     ]
   }
 
-  Video.paginate(query, {page: req.query.page, limit: req.query.limit}, (err, result) => {
+  Video.paginate(query, { page: req.query.page, limit: req.query.limit }, (err, result) => {
     const title = [];
     const choreographer = [];
     const url = [];
@@ -136,28 +147,28 @@ router.post('/dashboard', (req, res) => {
       req.flash('error_msg', "Looks like we don't have that video yet!");
       res.redirect('/dashboard?page=1&limit=15');
     } else {
-    for (let i = 0; i < result.docs.length; i++) {
-      title[i] = result.docs[i].title;
-      choreographer[i] = result.docs[i].choreographer;
-      url[i] = result.docs[i].url;
-      level[i] = result.docs[i].level;
-      thumbnail[i] = result.docs[i].thumbnail;
-      id[i] = result.docs[i].id;
-    }
-    res.render('../views/results', {
-      count: result.total,
-      username: req.session.passport.user.displayName,
-      videos: result.docs,
-      title: title,
-      choreographer: choreographer,
-      url: url,
-      id: id,
-      level: level,
-      thumbnail: thumbnail,
-      currentPage: result.page,
-      pageCount: result.pages,
-      pages: paginate.getArrayPages(req)(3, result.pages, req.query.page)
-    })
+      for (let i = 0; i < result.docs.length; i++) {
+        title[i] = result.docs[i].title;
+        choreographer[i] = result.docs[i].choreographer;
+        url[i] = result.docs[i].url;
+        level[i] = result.docs[i].level;
+        thumbnail[i] = result.docs[i].thumbnail;
+        id[i] = result.docs[i].id;
+      }
+      res.render('../views/results', {
+        count: result.total,
+        username: req.session.passport.user.displayName,
+        videos: result.docs,
+        title: title,
+        choreographer: choreographer,
+        url: url,
+        id: id,
+        level: level,
+        thumbnail: thumbnail,
+        currentPage: result.page,
+        pageCount: result.pages,
+        pages: paginate.getArrayPages(req)(3, result.pages, req.query.page)
+      })
     }
   })
 })
@@ -167,7 +178,7 @@ router.post('/upload', (req, res) => {
   let errors = [];
 
   // Check required fields
-  if (title == '' || choreographer == '' || thumbnail == '' || url == '' || id == '' || publishedDate == '' || length == '' ||  language == '' || level == '' || genre == '' || purpose == '' || mood == '') {
+  if (title == '' || choreographer == '' || thumbnail == '' || url == '' || id == '' || publishedDate == '' || length == '' || language == '' || level == '' || genre == '' || purpose == '' || mood == '') {
     errors.push({ msg: 'Please fill in all fields' });
   }
 
@@ -194,15 +205,17 @@ router.post('/upload', (req, res) => {
   }
   Video.updateMany(
     {},
-    { $addToSet: {
-      length: ["any"],
-      language: ["any"],
-      level: ["any"],
-      genre: ["any"],
-      purpose: ["any"],
-      mood: ["any"]
-    } },
-    function(err, result) {
+    {
+      $addToSet: {
+        length: ["any"],
+        language: ["any"],
+        level: ["any"],
+        genre: ["any"],
+        purpose: ["any"],
+        mood: ["any"]
+      }
+    },
+    function (err, result) {
       if (err) {
         console.log(err);
       }
