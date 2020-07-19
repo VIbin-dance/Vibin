6f37a3f073dcba4b945d74dbc90ef45c77694b0c
Shota Inoue <shota.inoue@vibin.tokyo>
2020-07-19 10:01:54 +0900

    fixed dashboard design

--- routes/users.js
+++ routes/users.js
@@ -17,44 +17,40 @@ router.get('/newsletter', (req, res) => {
 router.post('/newsletter', (req, res) => {
     const { email } = req.body;
 
-    User.findOne({ email: email }, (err, user) => {
+    User.findOneAndUpdate({ email: email }, { subscription: true }, (err, user) => {
         if (!user) {
             req.flash('error_msg', 'There is no such user');
             res.redirect('/dashboard?page=1&limit=15');
         } else {
-            Video.find({ language: 'en' }, (err, result) => {
-                const smtp = nodemailer.createTransport({
-                    service: 'Gmail',
-                    secure: false,
-                    auth: {
-                        user: "Vibin'",
-                        pass: "Shota0130"
-                    },
-                    tls: {
-                        rejectUnauthorized: false
-                    }
-                });
+            const smtp = nodemailer.createTransport({
+                service: 'Gmail',
+                secure: false,
+                auth: {
+                    user: "2020.shota.inoue@uwcisak.jp",
+                    pass: "Shota0130"
+                },
+                tls: {
+                    rejectUnauthorized: false
+                }
+            });
 
-                const mailOptions = {
-                    to: user.email,
-                    from: '2020.shota.inoue@uwcisak.jp',
-                    subject: "Thank you for subscribing!",
-                    html: `
-                    <h1>Today's highlights</h1><h2>${result[0].title}</h2><a href="https://www.vibin.tokyo/player/${result[0].id}"><img src="${result[0].thumbnail}"></a>
-                    <h2>${result[1].title}</h2><a href="https://www.vibin.tokyo/player/${result[1].id}"><img src="${result[1].thumbnail}"></a>
-                    <h2>${result[2].title}</h2><a href="https://www.vibin.tokyo/player/${result[2].id}"><img src="${result[2].thumbnail}"></a>
+            const mailOptions = {
+                to: user.email,
+                from: '2020.shota.inoue@uwcisak.jp',
+                subject: "Thank you for subscribing!",
+                html: `
+                    <h1>Today's highlights</h1>
                     `
-                };
+            };
 
-                smtp.sendMail(mailOptions, (err, response) => {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        req.flash('success_msg', `We've sent ${email} an email!`);
-                        res.redirect('/dashboard?page=1&limit=15');
-                    }
-                });
-            }).limit(3)
+            smtp.sendMail(mailOptions, (err, response) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    req.flash('success_msg', `We've sent ${email} an email!`);
+                    res.redirect('/dashboard/-1?page=1&limit=15');
+                }
+            });
         }
     })
 })
