54a9240d4cc71c59bbcce1a8eb2a8db0422ed543
Shota Inoue <shota.inoue@vibin.tokyo>
2020-08-17 13:58:16 +0900

    more responsive fixes

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
     onlyDevs: (req, res, next) => {
-        if (req.user.emails[0].value == '925shota@gmail.com' || req.user.emails[0].value == '2020.shota.inoue@uwcisak.jp' || req.user.emails[0].value == 'so.gorman@vibin.tokyo') {
+        if (req.user.emails[0].value == '925shota@gmail.com' || req.user.emails[0].value == 'hibiki.ichihara@vibin.tokyo' || req.user.emails[0].value == 'so.gorman@vibin.tokyo') {
             return next();
           } else {
             req.flash('error_msg', "Sorry this is not yet available to users!");
