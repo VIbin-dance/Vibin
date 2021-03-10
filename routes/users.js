a4707c99c53ce20903f69d2eae7453be11b1ce1a
unknown <shota.inoue@vibin.tokyo>
2021-03-10 14:51:26 +0900

    improved profile edit usability

--- routes/users.js
+++ routes/users.js
@@ -101,6 +101,7 @@ router.get('/profile/edit', ensureAuthenticated, (req, res) => {
                 email: user.email,
                 firstName: user.name.givenName,
                 lastName: user.name.familyName,
+                curUsername: user.username,
                 username: user.username
             })
         }
@@ -162,6 +163,7 @@ router.post('/profile/edit', ensureAuthenticated, upload.single('userPhotoDef'),
             email: user.email,
             user,
             username,
+            curUsername: user.username,
             bio,
             level,
             purpose,
@@ -176,114 +178,114 @@ router.post('/profile/edit', ensureAuthenticated, upload.single('userPhotoDef'),
 
 })
 
-router.get('/:id', ensureAuthenticated, (req, res) => {
-    User.findOne({ _id: req.params.id }, (err, user) => {
-        User.findOne({ email: req.user._json.email }, async(err, currentUser) => {
+// router.get('/:id', ensureAuthenticated, (req, res) => {
+//     User.findOne({ _id: req.params.id }, (err, user) => {
+//         User.findOne({ email: req.user._json.email }, async(err, currentUser) => {
 
-            const likedVid = await Video.find({ 'like.id': user._id.toString() }).exec()
+//             const likedVid = await Video.find({ 'like.id': user._id.toString() }).exec()
 
-            if (!user) {
-                req.flash('error_msg', res.__('msg.error.noUser'));
-                res.redirect('/dashboard/-1?page=1&limit=15');
-            } else {
-                res.render('users', {
-                    likedVid: likedVid,
-                    user: user,
-                    currentUser: currentUser,
-                    followingCount: user.following.length,
-                    followerCount: user.follower.length,
-                    bio: user.bio,
-                    following: user._id,
-                    follower: currentUser._id,
-                    userPhoto: currentUser.userPhoto,
-                    userPhotoDef: currentUser.userPhotoDef,
-                    proPhoto: user.userPhoto,
-                    proPhotoDef: user.userPhotoDef,
-                    firstName: user.name.givenName,
-                    lastName: user.name.familyName,
-                    username: user.username
-                });
-            }
-        })
-    })
-})
+//             if (!user) {
+//                 req.flash('error_msg', res.__('msg.error.noUser'));
+//                 res.redirect('/dashboard/-1?page=1&limit=15');
+//             } else {
+//                 res.render('users', {
+//                     likedVid: likedVid,
+//                     user: user,
+//                     currentUser: currentUser,
+//                     followingCount: user.following.length,
+//                     followerCount: user.follower.length,
+//                     bio: user.bio,
+//                     following: user._id,
+//                     follower: currentUser._id,
+//                     userPhoto: currentUser.userPhoto,
+//                     userPhotoDef: currentUser.userPhotoDef,
+//                     proPhoto: user.userPhoto,
+//                     proPhotoDef: user.userPhotoDef,
+//                     firstName: user.name.givenName,
+//                     lastName: user.name.familyName,
+//                     username: user.username
+//                 });
+//             }
+//         })
+//     })
+// })
 
-router.post('/:id', ensureAuthenticated, (req, res) => {
-    const { following, follower, action } = req.body;
+// router.post('/:id', ensureAuthenticated, (req, res) => {
+//     const { following, follower, action } = req.body;
 
-    try {
-        if (action == 'follow') {
-            User.findByIdAndUpdate(follower, { $push: { following: [following] } }).exec();
-            User.findByIdAndUpdate(following, { $push: { follower: [follower] } }).exec()
-                .then(function(user) {
-                    req.flash('success_msg', res.__('msg.success.followed'));
-                    res.redirect(`/users/${user._id}`);
-                })
-                .catch(err => console.log(err));
-        } else if (action == 'unfollow') {
-            User.findByIdAndUpdate(follower, { $pull: { following: following } }).exec();
-            User.findByIdAndUpdate(following, { $pull: { follower: follower } }).exec()
-                .then(function(user) {
-                    req.flash('success_msg', res.__('msg.success.unfollowed'));
-                    res.redirect(`/users/${user._id}`);
-                })
-                .catch(err => console.log(err));
-        }
-    } catch (err) {
-        console.log(err);
-    }
-})
+//     try {
+//         if (action == 'follow') {
+//             User.findByIdAndUpdate(follower, { $push: { following: [following] } }).exec();
+//             User.findByIdAndUpdate(following, { $push: { follower: [follower] } }).exec()
+//                 .then(function(user) {
+//                     req.flash('success_msg', res.__('msg.success.followed'));
+//                     res.redirect(`/users/${user._id}`);
+//                 })
+//                 .catch(err => console.log(err));
+//         } else if (action == 'unfollow') {
+//             User.findByIdAndUpdate(follower, { $pull: { following: following } }).exec();
+//             User.findByIdAndUpdate(following, { $pull: { follower: follower } }).exec()
+//                 .then(function(user) {
+//                     req.flash('success_msg', res.__('msg.success.unfollowed'));
+//                     res.redirect(`/users/${user._id}`);
+//                 })
+//                 .catch(err => console.log(err));
+//         }
+//     } catch (err) {
+//         console.log(err);
+//     }
+// })
 
-router.get('/:type/:id', ensureAuthenticated, (req, res) => {
-    User.findOne({ _id: req.params.id }, async(err, user) => {
+// router.get('/:type/:id', ensureAuthenticated, (req, res) => {
+//     User.findOne({ _id: req.params.id }, async(err, user) => {
 
-        const currentUser = await User.findOne({ email: req.user._json.email }).exec();
+//         const currentUser = await User.findOne({ email: req.user._json.email }).exec();
 
-        if (!user) {
-            req.flash('error_msg', res.__('msg.error.noUser'));
-            res.redirect('/dashboard/-1?page=1&limit=15');
-        }
+//         if (!user) {
+//             req.flash('error_msg', res.__('msg.error.noUser'));
+//             res.redirect('/dashboard/-1?page=1&limit=15');
+//         }
 
-        if (req.params.type == 'following') {
-            const following = [];
+//         if (req.params.type == 'following') {
+//             const following = [];
 
-            for (let i = 0; i < user.following.length; i++) {
-                following[i] = await User.findById(user.following[i], 'username').exec();
-            }
+//             for (let i = 0; i < user.following.length; i++) {
+//                 following[i] = await User.findById(user.following[i], 'username').exec();
+//             }
 
-            const currentUser = await User.findOne({ email: req.user._json.email }).exec();
+//             const currentUser = await User.findOne({ email: req.user._json.email }).exec();
 
-            res.render('follow', {
-                follow: following,
-                type: req.params.type,
-                userPhoto: currentUser.userPhoto,
-                userPhotoDef: currentUser.userPhotoDef,
-                followCount: user.following.length,
-                username: user.username
-            });
-        } else if (err) {
-            console.log(err);
-        }
+//             res.render('follow', {
+//                 follow: following,
+//                 type: req.params.type,
+//                 userPhoto: currentUser.userPhoto,
+//                 userPhotoDef: currentUser.userPhotoDef,
+//                 followCount: user.following.length,
+//                 username: user.username
+//             });
+//         } else if (err) {
+//             console.log(err);
+//         }
 
-        if (req.params.type == 'follower') {
-            const follower = [];
+//         if (req.params.type == 'follower') {
+//             const follower = [];
 
-            for (let i = 0; i < user.follower.length; i++) {
-                follower[i] = await User.findById(user.follower[i], 'username').exec();
-            }
+//             for (let i = 0; i < user.follower.length; i++) {
+//                 follower[i] = await User.findById(user.follower[i], 'username').exec();
+//             }
 
-            res.render('follow', {
-                follow: follower,
-                type: req.params.type,
-                userPhoto: currentUser.userPhoto,
-                userPhotoDef: currentUser.userPhotoDef,
-                followCount: user.follower.length,
-                username: user.username
-            });
-        } else if (err) {
-            console.log(err);
-        }
-    })
-})
+//             res.render('follow', {
+//                 follow: follower,
+//                 type: req.params.type,
+//                 userPhoto: currentUser.userPhoto,
+//                 userPhotoDef: currentUser.userPhotoDef,
+//                 followCount: user.follower.length,
+//                 username: user.username
+//             });
+//         } else if (err) {
+//             console.log(err);
+//         }
+//     })
+// })
 
 module.exports = router;
