706ee39fe6adb2b5562c76c4e87c4adbdbc0a1b0
unknown <shota.inoue@vibin.tokyo>
2021-08-16 01:53:48 +0900

    fixed minor bug

--- routes/index.js
+++ routes/index.js
@@ -267,6 +267,7 @@ router.get("/reservation/:id", ensureAuthenticated, async (req, res) => {
 
     if (req.session.user.lesson && req.session.user.lesson.includes(lesson._id.toString()) === true) {
         res.render('success', {
+            stripeid: req.session.user.stripeID,
             user: req.session.user,
             params: req.params.id,
             lesson: lesson,
@@ -277,6 +278,7 @@ router.get("/reservation/:id", ensureAuthenticated, async (req, res) => {
         })
     } else if (lesson.price === 0) {
         res.render('reservation', {
+            stripeid: req.session.user.stripeID,
             id: undefined,
             params: req.params.id,
             lesson: lesson,
@@ -317,6 +319,7 @@ router.get("/reservation/:id", ensureAuthenticated, async (req, res) => {
         });
 
         res.render("reservation", {
+            stripeid: req.session.user.stripeID,
             id: session.id,
             lesson: lesson,
             price: price,
